bcc02f60951f56c97349d355d3c439be0e2ae59d
Alberto <algaru33@hotmail.com>
2019-09-25 18:10:06 +0200

    Section5.ErrorHandling. bugs fixed

--- ChecklistApp-SPA/src/app/_services/error.interceptor.ts
+++ ChecklistApp-SPA/src/app/_services/error.interceptor.ts
@@ -10,22 +10,19 @@ export class ErrorInterceptor implements HttpInterceptor {
             catchError(error => {
                 if (error instanceof HttpErrorResponse) {
                     if (error.status === 401) {
-                        console.log('applicationError type 0');
                         return throwError(error.statusText);
                     }
                     const applicationError = error.headers.get('Application-Error');
                     if (applicationError) {
-                        console.log('applicationError type 1');
                         console.error(applicationError);
                         return throwError(applicationError);
                     }
-                    const serverError = error.error.errors;
+                    const serverError = error.error;
                     let modalStateErrors = '';
-                    if (serverError && typeof serverError === 'object') {
-                        console.log('applicationError type 2');
-                        for (const key in serverError) {
-                            if (serverError[key]) {
-                                modalStateErrors += serverError[key] + '\n';
+                    if (serverError.errors && typeof serverError.errors === 'object') {
+                        for (const key in serverError.errors) {
+                            if (serverError.errors[key]) {
+                                modalStateErrors += serverError.errors[key] + '\n';
                             }
                         }
                     }
