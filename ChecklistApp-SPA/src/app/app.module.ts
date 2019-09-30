79b662f1c6e5b07d4f72cc643239502c58849932
Alberto <algaru33@hotmail.com>
2019-09-30 16:53:28 +0200

    alertify service implemented

--- ChecklistApp-SPA/src/app/app.module.ts
+++ ChecklistApp-SPA/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
    declarations: [
@@ -27,7 +28,9 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       FormsModule
    ],
    providers: [
-      ErrorInterceptorProvider
+      AuthService,
+      ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
