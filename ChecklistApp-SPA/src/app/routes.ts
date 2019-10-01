e5e123f498e0612ef012e51fdcd99025d04475ba
Alberto <algaru33@hotmail.com>
2019-10-01 13:31:08 +0200

    AuthGuard

--- ChecklistApp-SPA/src/app/routes.ts
+++ ChecklistApp-SPA/src/app/routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { ClientListComponent } from './client-list/client-list.component';
 import { ChecklistListComponent } from './checklist-list/checklist-list.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'clients', component: ClientListComponent},
+  { path: 'clients', component: ClientListComponent, canActivate: [AuthGuard]},
   { path: 'checklists', component: ChecklistListComponent},
   { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
